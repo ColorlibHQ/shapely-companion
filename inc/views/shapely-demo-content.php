9c7d2c159c2d31d392542049d892679d074667f5
Giucu <giucu91@yahoo.com>
2017-07-17 16:10:30 +0300

    #213

--- inc/views/shapely-demo-content.php
+++ inc/views/shapely-demo-content.php
@@ -5,21 +5,29 @@ if ( ! defined( 'WPINC' ) ) {
 }
 ?>
 <div id="demo_content">
-	<div class="import-full-content">
-		<p>
-			<a href="#" class="button button-primary"
-			   data-action="import-all"><?php _e( 'I want my site to look like your demo', 'shapely-companion' ) ?></a>
-			<span class="spinner"></span>
-		</p>
-		<div class="updated-message"><p><?php _e( 'Content Imported', 'shapely-companion' ) ?></p></div>
-	</div>
-	<div>
-		<p><?php _e( 'I want only to import demo widgets', 'shapely-companion' ) ?></p>
-		<p>
-			<a href="#" class="button button-secondary"
-			   data-action="import-widgets"><?php _e( 'Import Widgets', 'shapely-companion' ) ?></a>
-			<span class="spinner"></span>
-		</p>
-		<div class="updated-message"><p><?php _e( 'Content Imported', 'shapely-companion' ) ?></p></div>
-	</div>
+	
+		<?php if ( ! Shapely_Notify_System::shapely_has_plugin( 'shapely-companion' ) || ! Shapely_Notify_System::shapely_has_plugin( 'jetpack' ) || ! Shapely_Notify_System::shapely_has_plugin( 'contact-form-7' ) ): ?>
+			<div>
+				<p><?php _e( 'In order to import the demo content you need to complete all the recommended actions from above.', 'shapely-companion' ) ?></p>
+			</div>
+		<?php else : ?>
+			<div class="import-full-content">
+				<p>
+					<a href="#" class="button button-primary"
+					   data-action="import-all"><?php _e( 'I want my site to look like your demo', 'shapely-companion' ) ?></a>
+					<span class="spinner"></span>
+				</p>
+				<div class="updated-message"><p><?php _e( 'Content Imported', 'shapely-companion' ) ?></p></div>
+			</div>
+			<div>
+				<p><?php _e( 'I want only to import demo widgets', 'shapely-companion' ) ?></p>
+				<p>
+					<a href="#" class="button button-secondary"
+					   data-action="import-widgets"><?php _e( 'Import Widgets', 'shapely-companion' ) ?></a>
+					<span class="spinner"></span>
+				</p>
+				<div class="updated-message"><p><?php _e( 'Content Imported', 'shapely-companion' ) ?></p></div>
+			</div>
+		<?php endif ?>
+
 </div>
