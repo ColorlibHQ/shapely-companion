3fbac109e7e4f6d044e5d88ebfcf58332d1d5a87
Giucu <giucu91@yahoo.com>
2017-10-23 12:51:04 +0300

    Version Bump

--- shapely-companion.php
+++ shapely-companion.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Shapely Companion
  * Plugin URI:        https://colorlib.com/wp/themes/shapely/
  * Description:       Shapely Companion is a companion plugin for Shapely theme.
- * Version:           1.0.7
+ * Version:           1.2.0
  * Author:            Colorlib
  * Author URI:        https://colorlib.com
  * License:           GPL-2.0+
