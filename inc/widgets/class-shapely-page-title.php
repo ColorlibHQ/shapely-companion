3dd8d56c6f3af3e17b9864f378c08c25d0a00577
Giucu <giucu91@yahoo.com>
2018-02-08 16:54:12 +0200

    Fixed Typo

--- inc/widgets/class-shapely-page-title.php
+++ inc/widgets/class-shapely-page-title.php
@@ -12,7 +12,7 @@ class Shapely_Page_Title extends WP_Widget {
 			'description'                 => esc_html__( 'This widget is used only in pages with Template Widget', 'shapely-companion' ),
 			'customize_selective_refresh' => true,
 		);
-		parent::__construct( 'shapely-page-title', esc_html__( '[Shapely] Paget Title', 'shapely-companion' ), $widget_ops );
+		parent::__construct( 'shapely-page-title', esc_html__( '[Shapely] Page Title', 'shapely-companion' ), $widget_ops );
 	}
 
 	function widget( $args, $instance ) {
