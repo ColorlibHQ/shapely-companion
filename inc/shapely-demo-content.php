8fa39ef0df6dd0aada6c5b8aef6d1a6d04057a40
Giucu <giucu91@yahoo.com>
2018-02-09 14:31:03 +0200

    Added set front page to import content.

--- inc/shapely-demo-content.php
+++ inc/shapely-demo-content.php
@@ -100,13 +100,51 @@ function shapely_companion_import_content() {
 
 		} elseif ( 'import-widgets' == $_POST['import'] ) {
 			shapely_companion_add_default_widgets();
+		} elseif ( 'set-frontpage' == $_POST['import'] ) {
+			
+			$frontpage_title = __( 'Front Page', 'shapely-companion' );
+			$blog_title      = __( 'Blog', 'shapely-companion' );
+
+			$frontpage_id = wp_insert_post(
+				array(
+					'post_title'  => $frontpage_title,
+					'post_status' => 'publish',
+					'post_type'   => 'page',
+				)
+			);
+			$blog_id      = wp_insert_post(
+				array(
+					'post_title'  => $blog_title,
+					'post_status' => 'publish',
+					'post_type'   => 'page',
+				)
+			);
+
+			if ( - 1 != $frontpage_id ) {
+				update_post_meta( $frontpage_id, '_wp_page_template', 'page-templates/template-home.php' );
+			} // End if().
+
+			update_option( 'show_on_front', 'page' );
+			update_option( 'page_on_front', $frontpage_id );
+			update_option( 'page_for_posts', $blog_id );
+
 		}
 
 		update_option( 'shapely_imported_demo', true );
 
-		echo 'succes';
+		echo wp_json_encode(
+			array(
+				'status'  => true,
+				'message' => 'ok',
+			)
+		);
 	} else {
-		echo 'error';
+		echo wp_json_encode(
+			array(
+				'status'  => false,
+				'message' => 'nok',
+			)
+		);
 	}// End if().
 
 	exit();
