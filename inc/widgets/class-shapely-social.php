74b51de4f2b0534b5db87a1e165a7259eb23339b
unknown <cristi.ecmecian@yahoo.com>
2018-11-01 10:57:01 +0200

    fixes shapely #173

--- inc/widgets/class-shapely-social.php
+++ inc/widgets/class-shapely-social.php
@@ -6,13 +6,39 @@
  */
 class Shapely_Social extends WP_Widget {
 
-	private $defaults = array();
+	private $defaults;
+
+	private $social_icons = array(
+		'behance',
+		'codepen',
+		'dropbox',
+		'delicious',
+		'deviantart',
+		'digg',
+		'dribbble',
+		'facebook',
+		'flickr',
+		'github',
+		'instagram',
+		'linkedin',
+		'medium',
+		'pinterest',
+		'reddit',
+		'skype',
+		'slack',
+		'soundcloud',
+		'tumblr',
+		'tripadvisor',
+		'twitch',
+		'twitter',
+		'vimeo',
+		'youtube',
+	);
 
 	function __construct() {
-
 		$widget_ops = array(
-			'classname'                   => 'shapely-social',
-			'description'                 => esc_html__( 'shapely Social Widget', 'shapely-companion' ),
+			'classname'                   => 'widget-shapely-social',
+			'description'                 => esc_html__( 'Shapely Social Widget', 'shapely-companion' ),
 			'customize_selective_refresh' => true,
 		);
 		parent::__construct( 'shapely-social', esc_html__( '[Shapely] Social Widget', 'shapely-companion' ), $widget_ops );
@@ -23,65 +49,67 @@ class Shapely_Social extends WP_Widget {
 	}
 
 	function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
 
-		$instance = wp_parse_args( $instance, $this->defaults );
-
-		if ( ! function_exists( 'shapely_social_icons' ) ) {
-			echo __( 'This widget works only with Shapely theme. Please install and activate it first.', 'shapely-companion' );
+		$social_links = array();
+		foreach ( $this->social_icons as $social_icon ) {
+			$social_links[ $social_icon ] = isset( $instance[ 'page_' . $social_icon ] ) ? $instance[ 'page_' . $social_icon ] : '';
 		}
+
 		echo $args['before_widget'];
-		echo $args['before_title'];
-		echo esc_html( $instance['title'] );
-		echo $args['after_title'];
-
-		/**
-		 * Widget Content
-		 */
+		if ( '' != $title ) {
+			echo $args['before_title'] . esc_html( $title ) . $args['after_title'];
+		}
 		?>
-
-		<!-- social icons -->
-		<div class="social-icons sticky-sidebar-social">
-			<?php shapely_social_icons(); ?>
-		</div><!-- end social icons -->
-
+		<div class="shapely-social">
+			<?php foreach ( $this->social_icons as $social_icon ) : ?>
+				<?php if ( '' != $social_links[ $social_icon ] ) : ?>
+				<a class="shapely-social-link shapely-social-link--<?php echo esc_attr( $social_icon ); ?>" href="<?php echo esc_url( $social_links[ $social_icon ] ); ?>">
+					<span class="shapely-social-icon"></span>
+				</a>
+				<?php endif; ?>
+			<?php endforeach; ?>
+		</div>
 		<?php
-
 		echo $args['after_widget'];
 	}
 
-	/**
-	 * @param array $instance
-	 *
-	 * @return string|void
-	 */
-	function form( $instance ) {
-		if ( ! isset( $instance['title'] ) ) {
-			$instance['title'] = esc_html__( 'Follow us', 'shapely-companion' );
-		}
-		?>
 
-		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
-				<?php echo esc_html__( 'Title ', 'shapely-companion' ); ?>
-			</label>
-			<input type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" class="widefat"/>
-		</p>
+	function update( $new_instance, $old_instance ) {
+		$instance          = $old_instance;
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
 
-		<?php
-	}
-
-	/**
-	 * @param array $new_instance
-	 * @param array $old_instance
-	 *
-	 * @return array
-	 */
-	public function update( $new_instance, $old_instance ) {
-
-		$instance          = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? wp_kses_post( $new_instance['title'] ) : '';
+		foreach ( $this->social_icons as $social_icon ) :
+			$instance[ 'page_' . $social_icon ] = esc_url_raw( $new_instance[ 'page_' . $social_icon ] );
+		endforeach;
 
 		return $instance;
+	}
+
+	function form( $instance ) {
+
+		$defaults = $this->defaults;
+
+		foreach ( $this->social_icons as $social_icon ) :
+			$defaults[ 'page_' . $social_icon ] = '';
+		endforeach;
+
+		$instance = wp_parse_args( (array) $instance, $defaults );
+
+		?>
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title', 'shapely-companion' ); ?></label>
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" />
+		</p>
+
+		<?php foreach ( $this->social_icons as $social_icon ) : ?>
+			<p>
+				<label for="<?php echo esc_attr( $this->get_field_id( 'page_' . $social_icon ) ); ?>"><?php printf( esc_html__( '%s Page', 'shapely-companion' ), esc_html( ucfirst( $social_icon ) ) ); ?></label><br/>
+				<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'page_' . $social_icon ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'page_' . $social_icon ) ); ?>" type="text" value="<?php echo esc_attr( $instance[ 'page_' . $social_icon ] ); ?>" />
+			</p>
+		<?php endforeach; ?>
+
+		<?php
 	}
 
 }
